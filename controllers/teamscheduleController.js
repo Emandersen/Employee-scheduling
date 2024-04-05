05482800a24aec479a3574c67407897a85dfb843
Jesper Hjorth <jesper@plaiten.dk>
2024-04-05 14:50:22 +0200

    Added a table
    Made a table with weekdays

--- controllers/teamscheduleController.js
+++ controllers/teamscheduleController.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 
-function GET_team_schedule(req, res) {
-    res.render('team_schedule');
+
+
+function getCurrentWeek() {
+    const date = new Date();
+    const today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+    const firstDay = new Date(date.getFullYear(), 0, 1);
+    const dayOfYear = Math.round((today - firstDay) / 86400000);
+    const weekNumber = Math.ceil((dayOfYear + firstDay.getDay() + 1) / 7);
+  
+    return weekNumber;
+  }
+
+  function GET_team_schedule(req, res) {
+    res.render('team_schedule', { week_number: getCurrentWeek()});
 };
 
 module.exports = {
