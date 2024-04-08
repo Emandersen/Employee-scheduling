a372716c59de363249f1b0107736ed9f01b81dfa
Danmarkkugle <kje23@student.aau.dk>
2024-04-08 14:21:46 +0200

    Ekstra
    Ideen her var at lave en enkryption af kodeordet til siden, ved at tage kodeordet fra user.js (eller prøve på).

--- models/newUser.js
+++ models/newUser.js
@@ -22,3 +22,25 @@ module.exports = {
         })
     }
 }
+
+module.exports = {
+    loginUser: function(firstName, lastName, email, password, role, department, preferences, permission, callback) {
+        userModel.findone({email: email}).exec(function(error, user) {
+            if (error) {
+                callback({error: true})
+            } else if (!user) {
+                callback({error: true})
+            } else {
+                user.comparePassword(password, function(matchError, isMatch) {
+                    if (matchError) {
+                        callback({error: true})
+                    } else if (!isMatch) {
+                        callback({error: true})
+                    } else {
+                        callback({success: true})
+                    }
+                })
+            }
+        })
+    }
+}
