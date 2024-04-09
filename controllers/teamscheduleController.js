65fb6d0ad6011341078be21bdffc699d00b656a0
Emil Andersen <minnyecomputer.com@gmail.com>
2024-04-09 08:41:14 +0200

    experimental push

--- controllers/teamscheduleController.js
+++ controllers/teamscheduleController.js
@@ -3,24 +3,13 @@ var router = express.Router();
 var moment = require('moment');
 const Users = require('../models/user');
 const personalSchedule = require('./personalscheduleController');
+const dateHandler = require('../functions/dateHandler');
 
 function daysInAWeek() {
   const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
   return days;
 }
 
-
-
-
-function getCurrentWeek() {
-    const date = new Date();
-    const today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-    const firstDay = new Date(date.getFullYear(), 0, 1);
-    const dayOfYear = Math.round((today - firstDay) / 86400000);
-    const weekNumber = Math.ceil((dayOfYear + firstDay.getDay() + 1) / 7);
-  
-    return weekNumber;
-}
 
 async function GET_team_schedule(req, res) {
   const allNames = await Users.find().sort({lastName: 1}).exec();
@@ -31,7 +20,7 @@ async function GET_team_schedule(req, res) {
   }
 
   res.render('team_schedule', {
-    week_number: getCurrentWeek(),
+    week_number: dateHandler.getCurrentWeek(),
     names: arr,
     weekDays: daysInAWeek()
   });
