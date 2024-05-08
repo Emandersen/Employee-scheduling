c54c186c874586617712d357880fdbadf8b33752
Danmarkkugle <kje23@student.aau.dk>
2024-05-08 14:39:48 +0200

    Ikke færdigt prøver at lave den første soft con.

--- functions/softConstraints.js
+++ functions/softConstraints.js
@@ -1,10 +1,31 @@
+const moment = require('moment');
+
 // Soft constraints should always return a value between 0.01 and 0.2 , if the soft constraint critiria is not met
 // then the function should return the value as a minus value.
 
 // The more a nurse has worked the less prioritized they should be for emergency shifts
 function lessPrioritizedOverworkedNurse(schedule, user) {
+    let prioritized = 0;
+    let weeks = {};
+
+    const userSchedule = schedule.filter(shift => shift.email === user.email);
     
-    return true;
+    schedule = schedule.sort((a, b) => a.date - b.date);
+
+    for (let i = 0; i < schedule.length; i++) {
+        let beginningDay = new Date(userSchedule[i].date);
+        let endDay = new Date();
+
+        endDay.setDate(beginningDay.getDate() + 7);
+
+        for (let j = beginningDay; j < endDay; j++) {
+            
+        }
+    }
+
+
+    prioritized = -0.2;
+    return prioritized;
 }
 
 function example2() {
