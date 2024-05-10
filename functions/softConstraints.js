cae5b0e261d6b0b14ba12f3a0f6f207d1fae07c8
Danmarkkugle <kje23@student.aau.dk>
2024-05-10 14:35:19 +0200

    Functionen prototype
    Den første opbygning af soft constraint 1, der er mange ting der skal ses på, såsom priotezationen.

--- functions/softConstraints.js
+++ functions/softConstraints.js
@@ -6,25 +6,80 @@ const moment = require('moment');
 // The more a nurse has worked the less prioritized they should be for emergency shifts
 function lessPrioritizedOverworkedNurse(schedule, user) {
     let prioritized = 0;
-    let weeks = {};
+    let i = 0;
+    let check = 0;
 
     const userSchedule = schedule.filter(shift => shift.email === user.email);
     
     schedule = schedule.sort((a, b) => a.date - b.date);
 
-    for (let i = 0; i < schedule.length; i++) {
-        let beginningDay = new Date(userSchedule[i].date);
-        let endDay = new Date();
+    let beginningDay = new Date.getDate();
+    let endDay = new Date();
 
-        endDay.setDate(beginningDay.getDate() + 7);
+    endDay.setDate(beginningDay.getDate() + 7);
 
-        for (let j = beginningDay; j < endDay; j++) {
-            
-        }
+    for (i = 0; i < schedule.length; i++) {
+        if (beginningDay === new Date(userSchedule[i].date)) {
+            check = 1;
+            break;
+        } else if ((beginningDay + 1) === new Date(userSchedule[i].date)) {
+            check = 2;
+            break;
+        } else if ((beginningDay + 2) === new Date(userSchedule[i].date)) {
+            check = 3;
+            break;
+        } else if ((beginningDay + 3) === new Date(userSchedule[i].date)) {
+            check = 4;
+            break;
+        } else if ((beginningDay + 4) === new Date(userSchedule[i].date)) {
+            check = 5;
+            break;
+        } else if ((beginningDay + 5) === new Date(userSchedule[i].date)) {
+            check = 6;
+            break;
+        } else if ((beginningDay + 6) === new Date(userSchedule[i].date)) {
+            check = 7;
+            break;
+        } else if (endDay === new Date(userSchedule[i].date)) {
+            check = 8;
+            break;
+        } 
     }
 
+    if (check === 0)
+    {
+        prioritized = -0.2;
+        return prioritized;
+    }
 
-    prioritized = -0.2;
+    let shift = userSchedule[i];
+    const dateDay = ('0' + shift.date.getUTCDate()).slice(-2);
+    const dateMonth = ('0' + (shift.date.getUTCMonth() + 1)).slice(-2);
+    const dateYear = shift.date.getUTCFullYear();
+
+    const shiftStartTime = new Date(`${dateYear}-${dateMonth}-${dateDay}T${shift.startTime}:00Z`);
+    const shiftEndTime = new Date(`${dateYear}-${dateMonth}-${dateDay}T${shift.endTime}:00Z`);
+
+    let workhours = Math.floor((shiftStartTime - shiftEndTime) / (1000 * 60 * 60));
+
+    for (let j = (check - 1); j < 8; j++) {
+        let k = 1;
+        let malleableShift = userSchedule[i + k];
+        const malleableDay = ('0' + malleableShift.date.getUTCDate()).slice(-2);
+        const malleableMonth = ('0' + (malleableShift.date.getUTCMonth() + 1)).slice(-2);
+        const malleableYear = malleableShift.date.getUTCFullYear();
+
+        const malleableShiftStartTime = new Date(`${malleableYear}-${malleableMonth}-${malleableDay}T${malleableShift.startTime}:00Z`);
+        const malleableShiftEndTime = new Date(`${malleableYear}-${malleableMonth}-${malleableDay}T${malleableShift.endTime}:00Z`);
+
+        let malleableWorkhours = Math.floor((malleableShiftStartTime - malleableShiftEndTime) / (1000 * 60 * 60));
+
+        workhours = workhours + malleableWorkhours;
+    }
+
+    let diff = workhours / (7 * 24);
+
+    prioritized = diff * 0.2;
     return prioritized;
 }
 
